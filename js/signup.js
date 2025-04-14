19ee95088f552a537871392ec33f94f2557031b0
josoriov95 <josoriovaldes95@gmail.com>
2025-04-13 21:52:20 -0400

    Desarrollo de signup y estilo

---
+++ js/signup.js
@@ -0,0 +1,21 @@
+const signupForm = document.querySelector("#signup-form");
+
+signupForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const nombre = document.querySelector("#nombre").value;
+    const correo = document.querySelector("#correo").value;
+    const pass = document.querySelector("#pass").value;
+
+    const Users = JSON.parse(localStorage.getItem("users")) || [];
+    const isUserRegistered = Users.find(user => user.correo === correo);
+    if (isUserRegistered) {
+        return alert("Ya existe un usuario con el correo " + correo);
+    }
+
+    Users.push({nombre: nombre, correo: correo, pass: pass});
+    localStorage.setItem('users', JSON.stringify(Users));
+
+    alert("Registro Exitoso!");
+
+    window.location.href = "login.html";
+});
